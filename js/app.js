48aed825e76f1de2ee7184ea11eb6b49ac049be4
Juan Rendon <juanjoseflorezrendon124@gmail.com>
2024-03-23 17:24:24 -0500

    Header and navbar terminated

---
+++ js/app.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const navLinks = document.querySelector(".nav-links");
+  const links = navLinks.querySelectorAll("a");
+  const indicator = document.querySelector(".indicator-bar");
+
+  let activeLink = links[0];
+
+  function moveIndicator(e) {
+    indicator.style.width = `${e.offsetWidth}px`;
+    indicator.style.left = `${e.offsetLeft}px`;
+  }
+
+  moveIndicator(activeLink);
+
+  links.forEach((link) => {
+    link.addEventListener("mouseover", function () {
+      moveIndicator(this);
+    });
+    link.addEventListener("click", function () {
+      activeLink = this;
+    });
+  });
+
+  navLinks.addEventListener("mouseleave", function () {
+    moveIndicator(activeLink);
+  });
+});
